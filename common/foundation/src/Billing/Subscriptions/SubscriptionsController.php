247138358716b52c025418b2acff25086868ea87
AbdullahFaqeir <abdullah.alfaqeir@gmail.com>
2026-01-13 16:12:11 +0300

    - Fix creating a new subscription.

--- common/foundation/src/Billing/Subscriptions/SubscriptionsController.php
+++ common/foundation/src/Billing/Subscriptions/SubscriptionsController.php
@@ -1,13 +1,19 @@
-<?php namespace Common\Billing\Subscriptions;
+<?php
 
-use Common\Billing\Models\Price;
-use Common\Billing\Models\Product;
-use Common\Billing\Subscription;
-use Common\Core\BaseController;
-use Common\Database\Datasource\Datasource;
+namespace Common\Billing\Subscriptions;
+
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
+use Illuminate\Support\Carbon;
+use Common\Core\BaseController;
 use Illuminate\Validation\Rule;
+use Common\Billing\Subscription;
+use Common\Billing\Models\Price;
+use Illuminate\Http\JsonResponse;
+use Common\Billing\Models\Product;
+use Illuminate\Support\Facades\Auth;
+use Common\Database\Datasource\Datasource;
+use Illuminate\Contracts\Routing\ResponseFactory;
 
 class SubscriptionsController extends BaseController
 {
@@ -18,74 +24,78 @@ class SubscriptionsController extends BaseController
         $this->middleware('auth');
     }
 
-    public function index()
+    public function index(): Response|JsonResponse|ResponseFactory
     {
         $this->authorize('index', Subscription::class);
 
-        $dataSource = new Datasource(
-            $this->subscription->with(['user']),
-            $this->request->all(),
-        );
+        $dataSource = new Datasource($this->subscription->with(['user']),
+            $this->request->all(),);
 
-        $pagination = $dataSource->paginate()->toArray();
+        $pagination = $dataSource->paginate()
+                                 ->toArray();
 
         if (config('app.demo')) {
-            $pagination['data'] = $this->redactEmails(
-                $pagination['data'],
-                'user.email',
-            );
+            $pagination['data'] = $this->redactEmails($pagination['data'],
+                'user.email',);
         }
 
         return $this->success(['pagination' => $pagination]);
     }
 
-    public function store()
+    public function store(): Response|JsonResponse|ResponseFactory
     {
         $this->authorize('update', Subscription::class);
         $this->blockOnDemoSite();
 
         $data = $this->validate($this->request, [
-            'user_id' => 'required|exists:users,id|unique:subscriptions',
-            'renews_at' => 'required_without:ends_at|date|nullable',
-            'ends_at' => 'required_without:renews_at|date|nullable',
-            'product_id' => 'required|integer|exists:products,id',
-            'price_id' => 'required|integer|exists:prices,id',
+            'user_id'     => 'required|exists:users,id|unique:subscriptions',
+            'renews_at'   => 'required_without:ends_at|date|nullable',
+            'ends_at'     => 'required_without:renews_at|date|nullable',
+            'product_id'  => 'required|integer|exists:products,id',
+            'price_id'    => 'required|integer|exists:prices,id',
             'description' => 'string|nullable',
         ]);
+
+        $data['renews_at'] = Carbon::parse($data['renews_at']);
+        $data['ends_at'] = Carbon::parse($data['ends_at']);
 
         $subscription = $this->subscription->create($data);
 
         return $this->success(['subscription' => $subscription]);
     }
 
-    public function update(int $id)
+    public function update(int $id): Response|JsonResponse|ResponseFactory
     {
-        $subscription = Subscription::findOrFail($id);
+        $subscription = Subscription::query()
+                                    ->findOrFail($id);
 
         $this->authorize('show', $subscription);
         $this->blockOnDemoSite();
 
         $data = $this->validate($this->request, [
-            'user_id' => [
+            'user_id'     => [
                 'required',
                 'exists:users,id',
-                Rule::unique('subscriptions')->ignore($subscription->id),
+                Rule::unique('subscriptions')
+                    ->ignore($subscription->id),
             ],
-            'renews_at' => 'date|nullable',
-            'ends_at' => 'date|nullable',
-            'product_id' => 'required|integer|exists:products,id',
-            'price_id' => 'required|integer|exists:prices,id',
+            'renews_at'   => 'date|nullable',
+            'ends_at'     => 'date|nullable',
+            'product_id'  => 'required|integer|exists:products,id',
+            'price_id'    => 'required|integer|exists:prices,id',
             'description' => 'string|nullable',
         ]);
 
-        $subscription->fill($data)->save();
+        $subscription->fill($data)
+                     ->save();
 
         return $this->success(['subscription' => $subscription]);
     }
 
-    public function changePlan(int $id)
+    public function changePlan(int $id): Response|JsonResponse|ResponseFactory
     {
-        $subscription = Subscription::findOrFail($id);
+        $subscription = Subscription::query()
+                                    ->findOrFail($id);
         $this->authorize('show', $subscription);
 
         if ($subscription->user_id !== Auth::id()) {
@@ -94,7 +104,7 @@ class SubscriptionsController extends BaseController
 
         $data = $this->validate($this->request, [
             'newProductId' => 'required|integer|exists:products,id',
-            'newPriceId' => 'required|integer|exists:prices,id',
+            'newPriceId'   => 'required|integer|exists:prices,id',
         ]);
 
         $newProduct = Product::findOrFail($data['newProductId']);
@@ -102,13 +112,15 @@ class SubscriptionsController extends BaseController
 
         $subscription->changePlan($newProduct, $newPrice);
 
-        $user = $subscription->user()->first();
+        $user = $subscription->user()
+                             ->first();
         return $this->success(['user' => $user->load('subscriptions.product')]);
     }
 
-    public function cancel(int $id)
+    public function cancel(int $id): Response|JsonResponse|ResponseFactory
     {
-        $subscription = Subscription::findOrFail($id);
+        $subscription = Subscription::query()
+                                    ->findOrFail($id);
         $this->authorize('show', $subscription);
 
         if ($subscription->user_id !== Auth::id()) {
@@ -128,9 +140,10 @@ class SubscriptionsController extends BaseController
         return $this->success();
     }
 
-    public function resume(int $id)
+    public function resume(int $id): Response|JsonResponse|ResponseFactory
     {
-        $subscription = Subscription::findOrFail($id);
+        $subscription = Subscription::query()
+                                    ->findOrFail($id);
         $this->authorize('show', $subscription);
 
         if ($subscription->user_id !== Auth::id()) {
